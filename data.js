b0e0daa16c3b33e305fe001bd97c8092cd5530e8
atiyehgh <317891530+atiyehgh@users.noreply.github.com>
2026-09-13 12:23:51 +0000

    Auto-update data.js with new uploaded images

--- data.js
+++ data.js
@@ -132,10 +132,6 @@ const portfolio = {
   //استوری
   stories: [
     {
-      src: "images/uploads/stories/story1.jpg.jpg",
-      alt: "استوری Alka"
-    },
-    {
       src: "images/uploads/stories/Screenshot_2026-02-18-17-41-09-337_com.instagram.android.jpg",
       alt: "استوری Alka"
     }
