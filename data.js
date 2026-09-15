7a20cec23494aaa8cf3520cdb8362edb12b4300f
atiyehgh <317891530+atiyehgh@users.noreply.github.com>
2026-09-15 17:12:14 +0000

    Auto-update data.js with new uploaded images

--- data.js
+++ data.js
@@ -126,11 +126,14 @@ const portfolio = {
       src: "images/uploads/stories/Screenshot_2026-02-18-17-41-10-613_com.instagram.android.jpg",
       alt: "استوری Alka"
     },
-   {
+    {
       src: "images/uploads/stories/Screenshot_2026-02-18-17-41-11-821_com.instagram.android.jpg",
       alt: "استوری Alka"
     },
-
+    {
+      src: "images/uploads/stories/Screenshot_2026-02-18-17-41-13-091_com.instagram.android.jpg",
+      alt: "استوری Alka"
+    }
   ],
 
   timeline:[
